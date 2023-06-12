0358cae6a9184513b3d3de486fef0709b72c75ac
EgorenkovMV <egorenkovmv@yandex.ru>
2023-06-12 12:21:16 +0300

    ready for test hosting

--- SPWebsiteDjango/main/models.py
+++ SPWebsiteDjango/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    discipline = models.CharField('Предмет', max_length=50)
+    time = models.TimeField('Время')
+
+    def __str__(self):
+        return self.discipline
+
+    class Meta:
+        verbose_name = ' Занятие'
+        verbose_name_plural = ' Занятия'
--- SPWebsiteDjango/main/views.py
+++ SPWebsiteDjango/main/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-
+from .models import Entry
 
 def render_main_template(request):
-    data = {
-        'title': 'Main page',
-        'values': [1, {0: 'lol'}, 4.1415926],
-    }
-    return render(request,'main/main.html', data)
+    data = {'entries': Entry.objects.order_by('time')}
+    return render(request, 'main/main.html', data)
 
 
 def about(request):
-    return render(request,'main/about.html', {'title': 'About'})
+    return render(request, 'main/about.html', {'title': 'About'})
 
